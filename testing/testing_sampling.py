875f4c6934320808137008fc2c4e6912669bcf00
flavieq88 <qinflavie@gmail.com>
2024-04-17 13:41:26 -0400

    modified handstrength testing

--- testing/testing_sampling.py
+++ testing/testing_sampling.py
@@ -10,15 +10,16 @@ sys.path[0] = sys.path[0].strip(r"\testing")
 
 from winninghand import *
 from bot import handstrength
+
 from random import shuffle
 from time import perf_counter
 
-# to test if the sampling idea for hand strength is a repressentative measure of "real" hand strength
-#saves this data into text file "testing_sampling_analysis.txt"
+# to test if the sampling idea for hand strength is a representative measure of "real" hand strength
+# also testing if theres a difference between averaging many small samples or one big sample
+# saves this data into text file "testing_sampling_analysis.txt"
 
-n_iter = 1000
-n_samples = 10
 
+#helper function for finding true handstrength
 def pocketcombinations(deck):
     """Returns a list of tuples containing all possible pocket hands of deck, order doesnt matter"""
     ls = []
@@ -38,6 +39,7 @@ print("hand1 =", hand1, file=fp)
 print("community pile =", community, file=fp)
 print("Finding hand strength of hand1 (probability it wins)\n", file = fp)
 
+# first task: finding true handstrength
 print("Doing all combinations:", file = fp)
 start = perf_counter()
 wins = 0 # i decide that wins include ties because don't lose any money
@@ -52,32 +54,36 @@ for hand2 in pocketcombinations(deck):
 P = wins/total
 end = perf_counter()
 print("Number of operations =", total, file=fp)
-print("True probability =", P, file=fp)
+print("True hand strength =", P, file=fp)
 print("Time = ", end-start, "\n", file=fp)
 
 
-#random sampling:
-print("Sampling random combinations:", file=fp)
-print(f"Number of iterations = {n_iter}\n",file=fp)
-avg = 0
-for i in range(n_samples): #reapeat 10 times to get an idea
+# task 2: random sampling 1 sample of 1000 combinations
+# task 3: random sampling 10 sample of 100 combinations (averaging)
+tasks = [(1000, 1), (100, 10)] # (n_inter:n_samples)
+for n_iter, n_samples in tasks:
+    print(f"Sampling random combinations {n_samples} times:", file=fp)
+    print(f"Number of iterations = {n_iter}",file=fp)
     start = perf_counter()
-    wins = 0 # wins include ties because don't lose any money
-    total = 0
-    for i in range(n_iter): # number of iterations = size of random sample
-        shuffle(deck)
-        hand2 = deck[:2]
-        result = getwinner(hand1+community, hand2+community)
-        if result != "Loss":
-            wins +=1
-        total+=1
+    h = handstrength(hand1, community, n_iter, n_samples)
+    """
+    for i in range(n_samples): #repeat 10 times to get an idea
+        wins = 0 # wins include ties because don't lose any money
+        total = 0
+        for i in range(n_iter): # number of iterations = size of random sample
+            shuffle(deck)
+            hand2 = deck[:2]
+            result = getwinner(hand1+community, hand2+community)
+            if result != "Loss":
+                wins +=1
+            total+=1
+        print("Sampling probability =", wins/total, file=fp)
+        print("Percent error = {:3.2f}%".format(abs((wins/total-P)/P)*100), file=fp)
+        print("Time =", end-start,, file=fp)
+        avg += wins/total
+    """
     end = perf_counter()
-    print("Sampling probability =", wins/total, file=fp)
-    print("Percent error = {:3.2f}%".format(abs((wins/total-P)/P)*100), file=fp)
-    print("Time =", end-start, "\n", file=fp)
-    avg += wins/total
-avg = avg/10
-print("Average sampling probability =", avg, file = fp)
-print("Percent error (over {} samples) = {:3.2f}%".format(n_samples, abs(avg-P)/P*100), file = fp)
+    print("Average sampling probability =", h, file = fp)
+    print("Percent error over {} samples = {:3.2f}%\n".format(n_samples, abs(h-P)/P*100), file = fp)
 
 fp.close()
