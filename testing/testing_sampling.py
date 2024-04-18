81ade70e72f8d2ee1dbcdc26c57d6d22d6aa6ff1
flavieq88 <qinflavie@gmail.com>
2024-04-17 22:23:23 -0400

    fixed handstrength bug

--- testing/testing_sampling.py
+++ testing/testing_sampling.py
@@ -30,14 +30,16 @@ def pocketcombinations(deck):
 
 deck = [Card(i,j, True) for j in ["Club", "Diamond", "Heart", "Spade"] 
                      for i in range(2, 15)] #create a standard deck
-fp = open("testing/testing_sampling_analysis.txt", "a") #write results into a file
 
+fp = open("testing/testing_sampling_analysis.txt", "a") #write results into a file
+"""
 shuffle(deck)
 community = [deck.pop(), deck.pop(), deck.pop(), deck.pop(), deck.pop()]
 hand1 = [deck.pop(), deck.pop()]
 print("hand1 =", hand1, file=fp)
 print("community pile =", community, file=fp)
 print("Finding hand strength of hand1 (probability it wins)\n", file = fp)
+
 
 # first task: finding true handstrength
 print("Doing all combinations:", file = fp)
@@ -55,7 +57,7 @@ P = wins/total
 end = perf_counter()
 print("Number of operations =", total, file=fp)
 print("True hand strength =", P, file=fp)
-print("Time = ", end-start, "\n", file=fp)
+print(f"Time = {end-start} s\n", file=fp)
 
 
 # task 2: random sampling 1 sample of 1000 combinations
@@ -66,24 +68,61 @@ for n_iter, n_samples in tasks:
     print(f"Number of iterations = {n_iter}",file=fp)
     start = perf_counter()
     h = handstrength(hand1, community, n_iter, n_samples)
-    """
-    for i in range(n_samples): #repeat 10 times to get an idea
-        wins = 0 # wins include ties because don't lose any money
-        total = 0
-        for i in range(n_iter): # number of iterations = size of random sample
-            shuffle(deck)
-            hand2 = deck[:2]
-            result = getwinner(hand1+community, hand2+community)
-            if result != "Loss":
-                wins +=1
-            total+=1
-        print("Sampling probability =", wins/total, file=fp)
-        print("Percent error = {:3.2f}%".format(abs((wins/total-P)/P)*100), file=fp)
-        print("Time =", end-start,, file=fp)
-        avg += wins/total
-    """
     end = perf_counter()
     print("Average sampling probability =", h, file = fp)
-    print("Percent error over {} samples = {:3.2f}%\n".format(n_samples, abs(h-P)/P*100), file = fp)
+    #print("Percent error over {} samples = {:3.2f}%".format(n_samples, abs(h-P)/P*100), file = fp)
+    print(f"Time = {end-start} s\n", file = fp)
+
+"""
+# Now i want to try with 1 unknown community card + 2 unknown opponent pocket cards
+# Theoretically, 46 possible cards left in deck
+# 46 possibilities for the river and still 45*44/2 for opponent, so total 46*45*44/2 = 45540
+
+deck = [Card(i,j, True) for j in ["Club", "Diamond", "Heart", "Spade"] 
+                     for i in range(2, 15)] #create a standard deck
+
+shuffle(deck)
+print(deck)
+community = [] #2 unknown community card
+hand1 = [deck.pop(), deck.pop()]
+print("hand1 =", hand1, file=fp)
+print("community pile =", community, file=fp)
+print("Finding hand strength of hand1 (probability it wins)\n", file = fp)
+
+"""
+print("Doing all combinations:", file = fp)
+start = perf_counter()
+wins = 0 # i decide that wins include ties because don't lose any money
+total = 0
+
+for card in deck:
+    temp = deck
+    temp.remove(card)
+    river = [temp.pop()]
+    for hand2 in pocketcombinations(temp):
+        result = getwinner(hand1+community+river, list(hand2)+community+river)
+        if result != "Loss":
+            wins +=1
+        total+=1
+
+P = wins/total
+end = perf_counter()
+print("Number of operations =", total, file=fp)
+print("True hand strength =", P, file=fp)
+print(f"Time = {end-start} s\n", file=fp)
+"""
+# running 10 times 1000 iterations for unknown river
+n_iter = 1000
+n_samples = 4
+print(f"Sampling random combinations {n_samples} times:", file=fp)
+print(f"Number of iterations = {n_iter}",file=fp)
+for i in range(n_samples):
+    start = perf_counter()
+    h = handstrength(hand1, community, n_iter)
+    end = perf_counter()
+    print("Average sampling probability =", h, file = fp)
+    #print("Percent error over {} samples = {:3.2f}%".format(n_samples, abs(h-P)/P*100), file = fp)
+    print(f"Time = {end-start} s\n", file = fp)
+
 
 fp.close()
