bfd8b6b21f0e7d0193913c52d08c7c446a5ff4aa
flavieq88 <qinflavie@gmail.com>
2024-04-17 22:42:39 -0400

    fixed testing sampling program

--- bot.py
+++ bot.py
@@ -11,50 +11,31 @@ from players import Player
 from random import shuffle
 from table import *
 from winninghand import *
-"""
+
+
 def handstrength(pocketcards, community, n_iter=1000, n_samples=10):
-    '''Returns the hand strength of current hand and community cards'''
-    missing = 7 - len(community) #max is 7 unknowns, min will be 2 unknowns
-    d = [Card(i,j, True) for j in ["Club", "Diamond", "Heart", "Spade"] 
-                     for i in range(2, 15)] #create a full deck
-    for card in pocketcards+community:
-        d.remove(card) #remove the already known cards from the deck
-    average = 0
-    for i in range(n_samples): #repeat the sampling n_samples times
-        wins = 0
-        for j in range(n_iter): #do n_iter random hands
-            shuffle(d)
-            #build the unknown cards
-            rest = d[:missing] # total of n_missing cards chosen at random (shuffled)
-            opponent = rest[:2] # for sure 2 cards for oppinent pocket cards
-            community += rest[2:] #rest of the cards are the rest of community cards
-            if getwinner(pocketcards+community, opponent+community) != "Loss":
-                wins += 1
-        P = wins/n_iter
-        average += P
-    h = average/n_samples
-    return h
-"""
-def handstrength(pocketcards, community, n_iter=1000):
     """Returns the hand strength of current hand and community cards"""
     missing = 7 - len(community) #max is 7 unknowns, min will be 2 unknowns
     d = [Card(i,j, True) for j in ["Club", "Diamond", "Heart", "Spade"] 
                      for i in range(2, 15)] #create a full deck
     for card in pocketcards+community:
         d.remove(card) #remove the already known cards from the deck
-    wins = 0
-    for j in range(n_iter): #do n_iter random hands
-        shuffle(d)
-        #build the unknown cards
-        rest = d[:missing] # total of n_missing cards chosen at random (shuffled)
-        opponent = rest[:2] # for sure 2 cards for oppinent pocket cards
-        total_community = community+rest[2:] #rest of the cards are the rest of community cards
-        if getwinner(pocketcards+total_community, opponent+total_community) != "Loss":
-            wins += 1
-    P = wins/n_iter
-    return P                     
+    h= 0 #for total hand strength
+    for i in range(n_samples):
+        wins = 0
+        for j in range(n_iter): #do n_iter random hands
+            shuffle(d)
+            #build the unknown cards
+            rest = d[:missing] # total of n_missing cards chosen at random (shuffled)
+            opponent = rest[:2] # for sure 2 cards for oppinent pocket cards
+            total_community = community+rest[2:] #rest of the cards are the rest of community cards
+            if getwinner(pocketcards+total_community, opponent+total_community) != "Loss":
+                wins += 1
+        P = wins/n_iter
+        h += P
+    return h/n_samples #average                     
 #testing for handstrength in testing_sampling.py
-
+#note: i tried using my class Deck instead of a list for the deck, but it was significantly slower 
 
 
 class Bot(Player):
--- testing/testing_sampling.py
+++ testing/testing_sampling.py
@@ -32,7 +32,7 @@ deck = [Card(i,j, True) for j in ["Club", "Diamond", "Heart", "Spade"]
                      for i in range(2, 15)] #create a standard deck
 
 fp = open("testing/testing_sampling_analysis.txt", "a") #write results into a file
-"""
+
 shuffle(deck)
 community = [deck.pop(), deck.pop(), deck.pop(), deck.pop(), deck.pop()]
 hand1 = [deck.pop(), deck.pop()]
@@ -70,10 +70,10 @@ for n_iter, n_samples in tasks:
     h = handstrength(hand1, community, n_iter, n_samples)
     end = perf_counter()
     print("Average sampling probability =", h, file = fp)
-    #print("Percent error over {} samples = {:3.2f}%".format(n_samples, abs(h-P)/P*100), file = fp)
+    print("Percent error over {} samples = {:3.2f}%".format(n_samples, abs(h-P)/P*100), file = fp)
     print(f"Time = {end-start} s\n", file = fp)
 
-"""
+
 # Now i want to try with 1 unknown community card + 2 unknown opponent pocket cards
 # Theoretically, 46 possible cards left in deck
 # 46 possibilities for the river and still 45*44/2 for opponent, so total 46*45*44/2 = 45540
@@ -82,47 +82,45 @@ deck = [Card(i,j, True) for j in ["Club", "Diamond", "Heart", "Spade"]
                      for i in range(2, 15)] #create a standard deck
 
 shuffle(deck)
-print(deck)
-community = [] #2 unknown community card
+community = [deck.pop(), deck.pop(), deck.pop(), deck.pop()] #2 unknown community card
 hand1 = [deck.pop(), deck.pop()]
 print("hand1 =", hand1, file=fp)
 print("community pile =", community, file=fp)
 print("Finding hand strength of hand1 (probability it wins)\n", file = fp)
 
-"""
+
 print("Doing all combinations:", file = fp)
 start = perf_counter()
 wins = 0 # i decide that wins include ties because don't lose any money
 total = 0
 
 for card in deck:
-    temp = deck
+    temp = deck.copy()
     temp.remove(card)
-    river = [temp.pop()]
+    river = [card]
     for hand2 in pocketcombinations(temp):
         result = getwinner(hand1+community+river, list(hand2)+community+river)
         if result != "Loss":
             wins +=1
         total+=1
-
 P = wins/total
 end = perf_counter()
 print("Number of operations =", total, file=fp)
 print("True hand strength =", P, file=fp)
 print(f"Time = {end-start} s\n", file=fp)
-"""
+
 # running 10 times 1000 iterations for unknown river
 n_iter = 1000
-n_samples = 4
+n_samples = 10
 print(f"Sampling random combinations {n_samples} times:", file=fp)
 print(f"Number of iterations = {n_iter}",file=fp)
-for i in range(n_samples):
-    start = perf_counter()
-    h = handstrength(hand1, community, n_iter)
-    end = perf_counter()
-    print("Average sampling probability =", h, file = fp)
-    #print("Percent error over {} samples = {:3.2f}%".format(n_samples, abs(h-P)/P*100), file = fp)
-    print(f"Time = {end-start} s\n", file = fp)
+
+start = perf_counter()
+h = handstrength(hand1, community, n_iter, n_samples)
+end = perf_counter()
+print("Average sampling probability =", h, file = fp)
+print("Percent error over {} samples = {:3.2f}%".format(n_samples, abs(h-P)/P*100), file = fp)
+print(f"Time = {end-start} s\n", file = fp)
 
 
 fp.close()
